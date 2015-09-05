6e2006ed1034c666b271bb73d5cdf300fce9f363
Alberto G. Jácome <agjacome@gmail.com>
2015-09-05 07:41:00 +0200

    Implemen MergeSplitter object to split comparing and reference Fastas

---
+++ core/src/main/scala/funpep/FastaMergeSplitter.scala
@@ -0,0 +1,26 @@
+package funpep
+
+import java.nio.file.Path
+import java.util.UUID.randomUUID
+
+import scalaz._
+import scalaz.concurrent._
+import scalaz.stream._
+import scalaz.syntax.foldable._
+
+import data._
+import util.ops.path._
+import util.ops.foldable._
+
+
+object FastaMergeSplitter {
+
+  def mergeSplit[A](cmp: Fasta[A], ref: Fasta[A])(implicit ev: A ⇒ Compound): EphemeralStream[Fasta[A]] =
+    cmp.entries.toEphemeralStream.map(entry ⇒ Fasta(entry <:: ref.entries))
+
+  def saveSplits[A, F[_]: Foldable](directory: Path)(splits: F[Fasta[A]]): Process[Task, Unit] =
+    merge.mergeN(splits.toProcess map {
+      _.toFile(directory / randomUUID.toString + ".fasta")
+    })
+
+}
