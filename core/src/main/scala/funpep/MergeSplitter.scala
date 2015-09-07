578b0ca750d16f93c390e5a402636601edca1378
Alberto G. Jácome <agjacome@gmail.com>
2015-09-07 22:57:51 +0200

    Add apply/function syntax to MergeSplitter

--- core/src/main/scala/funpep/MergeSplitter.scala
+++ core/src/main/scala/funpep/MergeSplitter.scala
@@ -15,12 +15,18 @@ import util.ops.foldable._
 
 object MergeSplitter {
 
+  def apply[A](cmp: Fasta[A], ref: Fasta[A], dir: Path)(implicit ev: A ⇒ Compound): Process[Task, Path] =
+    saveSplits(dir) { mergeSplit(cmp, ref) }
+
   def mergeSplit[A](cmp: Fasta[A], ref: Fasta[A])(implicit ev: A ⇒ Compound): EphemeralStream[Fasta[A]] =
     cmp.entries.toEphemeralStream.map(entry ⇒ Fasta(entry <:: ref.entries))
 
-  def saveSplits[A, F[_]: Foldable](directory: Path)(splits: F[Fasta[A]]): Process[Task, Unit] =
-    merge.mergeN(splits.toProcess map {
-      _.toFile(directory / randomUUID.toString + ".fasta")
-    })
+  def saveSplit[A](directory: Path)(split: Fasta[A]): Process[Task, Path] = {
+    val path = directory / randomUUID.toString + ".fasta"
+    split.toFile(path).map(_ ⇒ path)
+  }
+
+  def saveSplits[A, F[_]: Foldable](directory: Path)(splits: ⇒ F[Fasta[A]]): Process[Task, Path] =
+    merge.mergeN { splits.toProcess map saveSplit(directory) }
 
 }
